3a5994f757e8b676a214671159f942394093e8bd
GLOBAL\I313136 <marius.branea@gmail.com>
2017-07-25 12:28:31 +0300

    teste scrise fara implementare

--- unit_test_example/index.js
+++ unit_test_example/index.js
@@ -1,7 +1,7 @@
 console.log("Unit test example start");
 
 var myValidator = require("./modules/passwordValidator");
-var password = "testparola"
+var password = "test"
 var valid = myValidator.validPassword(password);
 
 console.log("your password'  " + password + " ' is : " + valid);
--- unit_test_example/test/passwordValidatorTests.js
+++ unit_test_example/test/passwordValidatorTests.js
@@ -1,7 +1,17 @@
+
+var chai = require(chai);
+var passwordValidator = require("../modules/passwordValidator")
 describe("passwordValidator", function() {
    describe(".validPassword(password)", function() {
        it("should have minimum 8 char length", function(){
-           //Test Goes Here
+           var wrongpass = '1234567';
+           var valid = passwordValidator.validPassword(wrongpass);
+        //    chai checkk
+           chai.expect(valid).to.be.equal(false);
+
+           var goodPassword = '12345678';
+           var valid2 = passwordValidator.validPassword(goodpass);
+           chai.expect(valid).to.be.equal(true);
        });
    });
 });
